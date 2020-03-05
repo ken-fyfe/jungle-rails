8dc6d51382ed037a4536ce8d828accb4c337396f
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-03-05 16:05:43 +0000

    added some meat to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @number_products = Product.count
+    @number_categories = Category.count
+    puts 'number of products: ' + @number_products.to_s
+    puts 'number of categories: ' + @number_categories.to_s
   end
 end
